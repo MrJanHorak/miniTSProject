a4461ba1c572b5d5cc0804dbd9b222da969ad687
Jan Horak <janny_b_goode@hotmail.com>
2022-08-26 13:07:41 -0400

    add ul and eventlistener and append li function

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,18 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
+const list = document.getElementById("todolist");
 function handleSubmit(e) {
     e.preventDefault();
     console.log("SUBMITTED!");
+    const newTodoText = input.value;
+    const newLI = document.createElement("LI");
+    newLI.append(newTodoText);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
+    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
-});
+// btn.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
+const list = document.getElementById("todolist");
 
-function handleSubmit(e: SubmitEvent){
+function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log("SUBMITTED!")
+  console.log("SUBMITTED!");
+  const newTodoText = input.value;
+  const newLI = document.createElement("LI");
+  newLI.append(newTodoText);
+  list?.append(newLI);
+  input.value=""
 }
 
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
-btn.addEventListener("click", function() {
-  alert(input.value)
-  input.value=""
-})
+// btn.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
