920997ef9b8a6a8768229e0071fdf6c9e389acaf
Jan Horak <janny_b_goode@hotmail.com>
2022-08-26 13:01:52 -0400

    add form

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("SUBMITTED!");
+}
+form.addEventListener("submit", handleSubmit);
 btn.addEventListener("click", function () {
     alert(input.value);
     input.value = "";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
+function handleSubmit(e: SubmitEvent){
+  e.preventDefault();
+  console.log("SUBMITTED!")
+}
+
+form.addEventListener("submit", handleSubmit)
 
 btn.addEventListener("click", function() {
   alert(input.value)
